efa05c4b987f77106d560306cab5d3ea27ca9ba3
superche <35420957+superche@users.noreply.github.com>
2018-12-12 17:20:07 +0800

    Update rxjs-hooks.js

--- rxjs-hooks.js
+++ rxjs-hooks.js
@@ -1,1 +1,26 @@
-
+/**
+ * rxjs hooks
+ * Reference: git@github.com:LeetCode-OpenSource/rxjs-hooks.git
+ */
+import { from, Subject, BehaviorSubject } from "rxjs";
+function useEventCallback(callback, initialState) {
+  const [returnedCallback, setEventCallback] = useState(() => null);
+  const [state, setState] = useState(initialState);
+  const [state$] = useState(new BehaviorSubject(initialState));
+  useEffect(() => {
+    const event$ = new Subject();
+    setState(initialState);
+    setEventCallback(e => event$.next(e));
+    let value$ = callback(event$, state$);
+    const subscription = value$.subscribe(value => {
+      state$.next(value);
+      setState(value);
+    });
+    return () => {
+      subscription.unsubscribe();
+      event$.complete();
+      state$.complete();
+    };
+  }, []);
+  return [returnedCallback, state];
+}
